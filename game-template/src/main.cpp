87610ee0e79698f5b9be2685b2f42012ed141e2d
matty <mattreed1890@gmail.com>
2025-09-02 07:27:39 -0500

    On branch voxel  Your branch is up to date with 'origin/voxel'.
     Changes to be committed:
    	modified:   game-template/src/main.cpp
    	modified:   opeGL/src/cube.cpp

--- game-template/src/main.cpp
+++ game-template/src/main.cpp
@@ -1,12 +1,12 @@
 #include "game.h"
 #include "gamescene.h"
 
+
+
 int main() {
   start();
   createScene();
-  //addModel("resources/cup/scene.gltf", glm::vec3{0.0f, -1.0f, 0.0f},
-  //         glm::vec3{1.0f}, 0.0f, 0.25f);
-  addCube("resources/beige-textures/beige_wall_001_diff_4k.jpg", "resources/beige-textures/beige_wall_001_diff_4k.jpg", glm::vec3(0.0f, -1.5f, 0.0f), glm::vec3(1.0f), 0.0f, 1.0f); 
+  addCube("resources/beige-textures/beige_wall_001_diff_4k.jpg", "resources/beige-textures/beige_wall_001_diff_4k.jpg", glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(1.0f), 0.0f, 1.0f); 
   addPlane("resources/marble-textures/marble_01_diff_4k.jpg",
            "resources/marble-textures/marble_01_spec_4k.jpg",
            glm::vec3{0.0f, -2.0f, 0.0f}, glm::vec3{1.0f, 0.0f, 0.0f}, 90.f,
