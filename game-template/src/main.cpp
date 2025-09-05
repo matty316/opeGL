225e6f2c8d721e0b32548f84d03d7bcfca9415c2
matty <mattreed1890@gmail.com>
2025-09-05 12:10:30 -0500

    draw many chunks

--- game-template/src/main.cpp
+++ game-template/src/main.cpp
@@ -1,11 +1,16 @@
 #include "game.h"
 #include "gamescene.h"
 
+void createChunks() {
+  size_t size = 16;
+  for (size_t x = 0; x < size; x++)
+    for (size_t z = 0; z < size; z++)
+      addChunk(glm::vec3(static_cast<float>(x), -1.0f, static_cast<float>(z)), glm::vec3(1.0f), 0.0f, 0.1f);
+}
+
 void buildScene() {
   createScene();
-  addChunk(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), 0.0f, 0.1f); 
-  //addChunk(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f), 0.0f, 0.1f); 
-  //addChunk(glm::vec3(1.0f, 1.0f, 0.0f), glm::vec3(1.0f), 0.0f, 0.1f); 
+  createChunks();
   addPlane("resources/marble-textures/marble_01_diff_4k.jpg",
            "resources/marble-textures/marble_01_spec_4k.jpg",
            glm::vec3{0.0f, -2.0f, 0.0f}, glm::vec3{1.0f, 0.0f, 0.0f}, 90.f,
